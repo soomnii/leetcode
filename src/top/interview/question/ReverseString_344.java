e3ea2ea9d82e3e6d444568a8fb37b18b56bc7433
soominlee <dev.2smlee@gmail.com>
2023-01-04 15:02:35 +0900

    add main method

--- src/top/interview/question/ReverseString_344.java
+++ src/top/interview/question/ReverseString_344.java
@@ -6,12 +6,21 @@ package top.interview.question;
 * */
 
 class ReverseString_344 {
-    public void reverseString(char[] s) {
+
+    public static void main(String[] args) {
+        char[] s = {'1','2','3','4','5'};
+         s = reverseString(s);
+         System.out.println(s);
+    }
+
+    public static char[] reverseString(char[] s) {
         int n = s.length;
         for ( int i = 0; i <= (n/2)-1; i++){
             char tmp = s[i];
             s[i] = s[n-1-i];
             s[n-1-i] = tmp;
         }
+
+        return s;
     }
 }
